0177ae0b5a2ba0093931e0e7ce7d8dfe26db14ae
Forrest JB Hartley <forrest@forrestbear.org>
2022-02-21 18:14:33 -0500

    refactoring notes_helper tool

--- python/notes_helper.py
+++ python/notes_helper.py
@@ -2,21 +2,29 @@
 
 import argparse
 import os
+import pathlib
 import sys
+
+
 
 
 def create_parent_dir():
     # creates a directory for note candidates
+    desktop = pathlib.Path.home() / 'Desktop'
     dir_name = "implementation_series"
+    dir_path = os.path.join(desktop, dir_name)
 
     try:
-        os.mkdir(dir_name)
-        print(f'Directory: {dir_name}\nStatus: Created')
+        os.mkdir(dir_path)
+        print(f'Directory: {dir_path}\nStatus: Created')
     except FileExistsError:
-        print(f'Directory: {dir_name}\nStatus: Cannot complete, directory exists')
+        print(f'Directory: {dir_path}\nStatus: Cannot complete, directory exists')
 
-def report_path():
+    return dir_path
+
+def report_path(dir_path):
     # reports the fully qualified directory path
+    print(f'Full path of working report path:/n{dir_path}')
     return
 
 def load_series():
@@ -30,6 +38,7 @@ def sanitize_data():
 def export_notes():
     # breaks csv into files in parent dir
     # {dir_name}/{record}.txt
+    # default glob is txt, support for html as well
     return
 
 def create_import_csv():
@@ -41,3 +50,17 @@ def create_relation_csv():
     # takes success file note ID & path and relates them to parent records
     # generates a csv for import
     return
+
+
+class DirectoryHelper(object):
+    # DirectoryHelper: object that creates/stores directory path
+
+    def __init__(self, HOME, directory):
+        self.HOME = HOME
+        self.directory = directory
+
+class CsvHelper(object):
+    # CsvHelper: object that creates/stores/loads csv files for loading
+
+    def __init__(self,) -> None:
+        pass
